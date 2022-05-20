649194a808c0f421bd0c119d29b0aea7d63962ad
Leandro Citro De Andrade <leandro.citro@dominos.com.br>
2022-05-20 20:00:24 -0300

    Validação de CPF customer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-const express = require ('express');
+const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
+app.use(express.json());
+
+const customers = [];
+
 app.post("/account", (request, response) => {
-    const cpf = request.body;
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ Error: "Customer already existes!" })
+    }
+
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
 });
 
 
-
-app.listem(3333);
+app.listen(3333);
